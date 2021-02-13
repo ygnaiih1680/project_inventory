f47f5ce37dfbffce2a8fc22f68a97151598c0de0
AiRini <ygnaiih1680@mju.ac.kr>
2021-02-13 10:52:40 +0900

    disable debug

--- project_inventory/settings.py
+++ project_inventory/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 KAKAO_REST_API_KEY = get_secret("KAKAO_REST_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
